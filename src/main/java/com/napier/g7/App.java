eec2a0c8a53501c2d3c92f97a33bfa50b9273931
KyawKoKoSan <kyawkokosan5@gmail.com>
2024-01-25 11:50:42 +0630

    Added comment in app java file

--- src/main/java/com/napier/g7/App.java
+++ src/main/java/com/napier/g7/App.java
@@ -1,9 +1,13 @@
+// Package declaration indicating the package structure
 package com.napier.g7;
 
+// Class declaration for the main application
 public class App
 {
+    // The main method, entry point for the application
     public static void main(String[] args)
     {
+        // Printing a simple greeting message to the console
         System.out.println("Hello World!");
     }
 }
